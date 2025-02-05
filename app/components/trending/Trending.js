8544dd2872444529719a07961b9a2593753bcd68
Oladimeji Seunayo Ezekiel <oladimejiseunayo@gmail.com>
2025-02-05 01:10:24 +0100

    slsl

--- app/components/trending/Trending.js
+++ app/components/trending/Trending.js
@@ -7,7 +7,7 @@ const Trending = () => {
       <div className={styles["Top-deals"]}>
         <h3>New Trending</h3>
 
-        <div id="wrapper" className={styles["Top-deal-cover"]}>
+        <div  className={styles["Top-deal-cover"]}>
           
           <TrendingProducts />
         </div>
