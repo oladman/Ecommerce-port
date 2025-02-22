6a3202a5d6c22b335a23aa56fc8124dac78a97ab
Oladimeji Seunayo Ezekiel <oladimejiseunayo@gmail.com>
2025-02-22 12:41:00 +0100

    smsm

--- app/components/Benefits/Benefit.js
+++ app/components/Benefits/Benefit.js
@@ -8,9 +8,9 @@ const Benefit = () => {
   return (
     <div className={styles['cover']}>
    
-    <BenefitCover icon={<TbTruckDelivery size={40} style={{ color:'red' }}  />} topic="FAST DELIVERY" text="Your order is on its way before you even blink! Ultra-fast delivery, guaranteed!" />
-    <BenefitCover icon={<AiFillSafetyCertificate size={40} style={{ color:'red' }} />} topic="ORDER PROTECTION" text="Your order is safeguarded under our advanced protection framework." />
-    <BenefitCover icon={<FaMoneyCheckDollar size={40} style={{ color:'red' }} />} topic="RETURN POLICY" text="Enjoy peace of mind with our up to 7-day return policy, terms and conditions apply!" />
+    <BenefitCover icon={<TbTruckDelivery size={40} style={{ color:'#1b4d3e' }}  />} topic="FAST DELIVERY" text="Your order is on its way before you even blink! Ultra-fast delivery, guaranteed!" />
+    <BenefitCover icon={<AiFillSafetyCertificate size={40} style={{ color:'#1b4d3e' }} />} topic="ORDER PROTECTION" text="Your order is safeguarded under our advanced protection framework." />
+    <BenefitCover icon={<FaMoneyCheckDollar size={40} style={{ color:'#1b4d3e' }} />} topic="RETURN POLICY" text="Enjoy peace of mind with our up to 7-day return policy, terms and conditions apply!" />
     </div>
   )
 }
